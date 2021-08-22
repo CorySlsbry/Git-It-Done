8407c2142a2b8809d98e447010c497194c07e688
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-08-22 18:04:01 -0500

    added user repo functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,16 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
-};
 
-getUserRepos();
+// gets data from link. THEN function response {response.json} to convert data. THEN function data.
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+getUserRepos("microsoft");
+//////////////////
